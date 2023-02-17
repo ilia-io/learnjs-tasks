bee169e0ac1e1814a65ff6fb34d1520ed1990413
ilia-io <likeapu@gmail.com>
2023-02-17 18:22:37 +0300

    task 2.10 complete

---
+++ 02-Основы JavaScript/2.10 Условное ветвление if/4.js
@@ -0,0 +1,10 @@
+let result;
+
+if (a + b < 4) {
+  result = 'Мало';
+} else {
+  result = 'Много';
+}
+
+//ответ
+a + b < 4 ? (result = 'Мало') : (result = 'Много');
---
+++ 02-Основы JavaScript/2.10 Условное ветвление if/5.js
@@ -0,0 +1,22 @@
+let message;
+
+if (login == 'Сотрудник') {
+  message = 'Привет';
+} else if (login == 'Директор') {
+  message = 'Здравствуйте';
+} else if (login == '') {
+  message = 'Нет логина';
+} else {
+  message = '';
+}
+
+//ответ
+
+let message2 =
+  login == 'Сотрудник'
+    ? 'Привет'
+    : login == 'Директор'
+    ? 'Здравствуйте'
+    : login == ''
+    ? 'Нет логина'
+    : '';
