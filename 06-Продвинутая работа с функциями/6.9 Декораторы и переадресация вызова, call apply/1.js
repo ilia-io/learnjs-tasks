256f42edeba9eaf494938179b9fb9ab02ec80632
ilia-io <likeapu@gmail.com>
2023-03-06 18:04:09 +0300

    complete task 6.9

---
+++ 06-Продвинутая работа с функциями/6.9 Декораторы и переадресация вызова, call apply/1.js
@@ -0,0 +1,35 @@
+// Декоратор-шпион
+// важность: 5
+// Создайте декоратор spy(func), который должен возвращать обёртку, которая сохраняет все вызовы функции в своём свойстве calls.
+
+// Каждый вызов должен сохраняться как массив аргументов.
+
+// Например:
+
+function work(a, b) {
+  console.log(a + b); // произвольная функция или метод
+}
+
+function spy(func) {
+  function wrapper(...args) {
+    // мы используем ...args вместо arguments для хранения "реального" массива в wrapper.calls
+    wrapper.calls.push(args);
+    return func.apply(this, args);
+  }
+
+  wrapper.calls = [];
+
+  return wrapper;
+}
+
+work = spy(work);
+
+work(1, 2); // 3
+work(4, 5); // 9
+
+for (let args of work.calls) {
+  console.log('call:' + args.join()); // "call:1,2", "call:4,5"
+}
+// P.S.: Этот декоратор иногда полезен для юнит-тестирования. Его расширенная форма – sinon.spy – содержится в библиотеке Sinon.JS.
+
+// Открыть песочницу с тестами для задачи
