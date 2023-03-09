6d05b13c11b4481de68160fda3a9aeaa0641e50a
ilia-io <likeapu@gmail.com>
2023-03-09 18:18:09 +0300

    complete task 6.10

---
+++ 06-Продвинутая работа с функциями/6.10 Привязка контекста к функции/3.js
@@ -0,0 +1,14 @@
+// Свойство функции после bind
+// важность: 5
+// В свойство функции записано значение. Изменится ли оно после применения bind? Обоснуйте ответ.
+
+function sayHi() {
+  console.log(this.name);
+}
+sayHi.test = 5;
+
+let bound = sayHi.bind({
+  name: 'Вася',
+});
+
+console.log(bound.test); // undefined
